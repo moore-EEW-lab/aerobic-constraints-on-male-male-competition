7625a775038f3e4910079f6f21805ea70b40fbfe
Evolution & Eco-Physiology of Wetlands <141089330+moore-EEW-lab@users.noreply.github.com>
2026-04-28 13:58:07 -0600

    Update analyses for hypoxia & locomotor endurance.R
    Updated analysis scripts for hypoxia & locomotor endurance now including a 3rd species

--- analyses for hypoxia & locomotor endurance.R
+++ analyses for hypoxia & locomotor endurance.R
@@ -7,7 +7,7 @@ library(MuMIn)
 library(emmeans)
 library(visreg)
 
-hyp <- read.csv('hypoxia.fligth.endurance.csv')
+hyp <- read.csv('hypoxia.flight.endurance.csv')
 
 end01 <- lm(duration ~ po2 + sp + po2:sp, data = hyp)
 summary(end01)
@@ -16,33 +16,38 @@ summary(end01)
 
 # Residuals:
     # Min      1Q  Median      3Q     Max 
-# -32.181  -9.163  -2.022  10.196  33.253 
+# -35.770 -10.207  -1.849  10.407  33.253 
 
 # Coefficients:
              # Estimate Std. Error t value Pr(>|t|)    
-# (Intercept) -116.5252    24.5256  -4.751 1.54e-05 ***
-# po2            1.7840     0.2229   8.004 9.74e-11 ***
-# splpull       99.5115    33.8318   2.941   0.0048 ** 
-# po2:splpull   -0.6940     0.3064  -2.265   0.0275 *  
+# (Intercept) -116.5252    25.0375  -4.654 1.35e-05 ***
+# po2            1.7840     0.2276   7.840 2.23e-11 ***
+# splpull       99.5115    34.5378   2.881  0.00515 ** 
+# spplong      -68.8807    37.9524  -1.815  0.07348 .  
+# po2:splpull   -0.6940     0.3128  -2.219  0.02946 *  
+# po2:spplong    0.5460     0.3434   1.590  0.11596    
 # ---
 # Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-# Residual standard error: 16.15 on 54 degrees of freedom
+# Residual standard error: 16.49 on 76 degrees of freedom
   # (3 observations deleted due to missingness)
-# Multiple R-squared:  0.6979,	Adjusted R-squared:  0.6811 
-# F-statistic: 41.59 on 3 and 54 DF,  p-value: 4.596e-14
+# Multiple R-squared:  0.7493,	Adjusted R-squared:  0.7328 
+# F-statistic: 45.44 on 5 and 76 DF,  p-value: < 2.2e-16
 
 confint(end01)
-                  # 2.5 %       97.5 %
-# (Intercept) -165.696097 -67.35424036
-# po2            1.337119   2.23090655
-# splpull       31.682876 167.34010675
-# po2:splpull   -1.308239  -0.07982113
+                   # 2.5 %       97.5 %
+# (Intercept) -166.3916086 -66.65872889
+# po2            1.3307983   2.23722776
+# splpull       30.7234555 168.29952690
+# spplong     -144.4694611   6.70815836
+# po2:splpull   -1.3169273  -0.07113329
+# po2:spplong   -0.1378823   1.22984241
 
 emtrends(end01, specs = 'sp', var = 'po2')
  # sp    po2.trend    SE df lower.CL upper.CL
- # lluc       1.78 0.223 54    1.337     2.23
- # lpull      1.09 0.210 54    0.669     1.51
+ # lluc       1.78 0.228 76    1.331     2.24
+ # lpull      1.09 0.215 76    0.663     1.52
+ # plong      2.33 0.257 76    1.818     2.84
 
 
 #### does wing loading, wing size, or body mass affect these patterns?
